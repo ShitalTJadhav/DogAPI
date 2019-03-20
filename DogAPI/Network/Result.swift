cfccde1d3f0e0a79d6a0c5d936734627c6df8623
Shital Jadhav <shital.jadhavt11@gmail.com>
2019-03-20 11:45:45 +0100

    added file for implement network api call

---
+++ DogAPI/Network/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  ActivitiesApp
+//
+//  Created by Tushar  Jadhav on 2019-01-16.
+//  Copyright © 2019 Shital  Jadhav. All rights reserved.
+//
+
+import Foundation
+
+enum Result<T, E: Error> {
+    case success(T)
+    case failure(E)
+}
